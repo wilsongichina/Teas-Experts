1e2132bfc4ac675471367b2a5531335a174af2e3
Vi <react.hunter23@gmail.com>
2023-07-11 05:02:22 +0800

    quote at homepage

--- src/components/ITStartupTwo/MainBanner.js
+++ src/components/ITStartupTwo/MainBanner.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 // Shape Images
@@ -8,7 +8,47 @@ import Shape3 from "../../images/shape3.svg"
 import Shape4 from "../../images/shape4.svg"
 import Shape5 from "../../images/shape5.png"
 
+const QUOTE_FORM_ENDPOINT = "https://teasexport-mail.com/quote";
+
 const MainBanner = () => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleQuoteSubmit = (e) => {
+    e.preventDefault();
+
+    const inputs = e.target.elements;
+    const data = {};
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].name) {
+        data[inputs[i].name] = inputs[i].value;
+      }
+    }
+    console.log('quote data:', data);
+    setSubmitted(true);
+/*
+    fetch(QUOTE_FORM_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Form response was not ok');
+        }
+
+        setSubmitted(true);
+      })
+      .catch((err) => {
+        // Submit the form manually
+        e.target.submit();
+      });
+      */
+  };
+
   return (
     <>
       <div className="main-banner">
@@ -42,60 +82,70 @@ const MainBanner = () => {
 
                 <div className="col-lg-5 offset-lg-1">
                   <div className="banner-form ml-3">
-                    <form>
-                      <div className="form-group">
-                        <label htmlFor="text">Your Name</label>
-                        <input
-                          type="text"
-                          name="customer_name"
-                          className="form-control"
-                          placeholder="Mary Johns"
-                        />
+                    {submitted ? (
+                      <div className="container">
+                        <div className="section-title">
+                          <h2>Thank you for reaching out to us</h2>
+                          <div className="bar"></div>
+                          <p>We appreciate your message, and our support team will respond to you shortly.</p>
+                        </div>
                       </div>
+                    ) : (
+                      <form id="contactForm" action={QUOTE_FORM_ENDPOINT} onSubmit={handleQuoteSubmit}>
+                        <div className="form-group">
+                          <label htmlFor="text">Your Name</label>
+                          <input
+                            type="text"
+                            name="customer_name"
+                            className="form-control"
+                            placeholder="Mary Johns"
+                          />
+                        </div>
 
-                      <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                          type="email"
-                          name="customer_email"
-                          className="form-control"
-                          placeholder="johns@email.com"
-                        />
-                      </div>
+                        <div className="form-group">
+                          <label htmlFor="email">Email</label>
+                          <input
+                            type="email"
+                            name="customer_email"
+                            className="form-control"
+                            placeholder="johns@email.com"
+                          />
+                        </div>
 
-                      <div className="form-group">
-                        <label htmlFor="whatsapp">WhatsApp Number</label>
-                        <input
-                          type="text"
-                          name="whatsapp_number"
-                          className="form-control"
-                          placeholder="+170280..."
-                        />
-                      </div>
+                        <div className="form-group">
+                          <label htmlFor="whatsapp">WhatsApp Number</label>
+                          <input
+                            type="text"
+                            name="whatsapp_number"
+                            className="form-control"
+                            placeholder="+170280..."
+                          />
+                        </div>
 
-                      <div className="form-group">
-                        <label htmlFor="type-task">Type of Task</label>
-                        <select className="form-select" name="task_type">
-                          <option value="Teas Exam">Teas Exam</option>
-                          <option value="Hesi A2 Exam">Hesi A2 Exam</option>
-                          <option value="GED">GED</option>
-                          <option value="Online Class">Online Class</option>
-                        </select>
-                      </div>
+                        <div className="form-group">
+                          <label htmlFor="type-task">Type of Task</label>
+                          <select className="form-select" name="task_type">
+                            <option value="Teas Exam">Teas Exam</option>
+                            <option value="Hesi A2 Exam">Hesi A2 Exam</option>
+                            <option value="GED">GED</option>
+                            <option value="Online Class">Online Class</option>
+                          </select>
+                        </div>
 
-                      <div className="form-group">
-                        <label htmlFor="budget">Your budget</label>
-                        <input
-                          type="number"
-                          name="quote_budget"
-                          className="form-control"
-                        />
-                      </div>
+                        <div className="form-group">
+                          <label htmlFor="budget">Your budget</label>
+                          <input
+                            type="number"
+                            name="quote_budget"
+                            className="form-control"
+                          />
+                        </div>
 
-                      <button type="submit" className="btn btn-primary">
-                        Get a Quote Now
-                      </button>
-                    </form>
+                        <button type="submit" className="btn btn-primary">
+                          Get a Quote Now
+                        </button>
+                      </form>
+                    )}
                   </div>
                 </div>
               </div>
